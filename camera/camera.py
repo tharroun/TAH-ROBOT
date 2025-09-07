fb13d4e8c48c1e9afced65ed5030c9b6978e62ed
tharroun <thad.harroun@gmail.com>
2025-09-07 07:38:04 -0400

    addding camera module

---
+++ camera/camera.py
@@ -0,0 +1,59 @@
+import cv2
+import time
+import pprint
+from picamera2 import Picamera2
+
+
+class Camera:
+    """
+     Picamera module 3
+    """
+    def __init__(
+        self, 
+        logfile: str = "PiCamMod3.conf"
+    ):
+        self.picam2 = Picamera2()
+
+        with open("PiCamMod3.conf","w") as fp:
+            pprint.pp(self.picam2.camera_controls,fp)
+            pprint.pp(self.picam2.sensor_modes,fp)
+
+        mode   = self.picam2.sensor_modes[0]
+        config = self.picam2.create_preview_configuration(main={'size': (2304,1296),
+                                                     'format': 'RGB888'})
+        self.picam2.align_configuration(config)
+        self.picam2.configure(config)
+
+        cv2.namedWindow("Camera", cv2.WINDOW_NORMAL)
+        cv2.setWindowProperty("Camera", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        #cv2.setWindowProperty("Camera", cv2.WND_PROP_TOPMOST, 0)
+
+        self.picam2.start()
+# ------------------------------------------
+
+# ------------------------------------------
+    def start(self):
+        t1 = time.time() 
+        while True:
+            im  = self.picam2.capture_array()
+            res = cv2.resize(im,(800,480),interpolation = cv2.INTER_CUBIC)
+            cv2.imshow("Camera", res)
+            t2 = time.time()
+            #print(1/(t2-t1))
+            t1 = t2
+            if cv2.waitKey(1)==ord('q'):
+                break
+        cv2.destroyAllWindows()
+# ------------------------------------------
+
+# ------------------------------------------
+    def deinit(self):
+        self.picam2.stop()
+        self.picam2.close()
+# ------------------------------------------
+
+if __name__ == "__main__":
+    my_camera = Camera()
+    my_camera.start()
+    my_camera.deinit()
+
---
+++ drive/drive.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+#coding=utf8
+import sys
+import asyncio
+import signal
+import functools
+import multiprocessing
+sys.path.append('/home/tah/GitHub/TAH-ROBOT')
+sys.path.append('/home/tah/GitHub/TAH-ROBOT/servos')
+from servos import Servos
+sys.path.append('/home/tah/GitHub/TAH-ROBOT/motors')
+from motors import Motors
+sys.path.append('/home/tah/GitHub/TAH-ROBOT/gamepad')
+from gamepad import Gamepad
+sys.path.append('/home/tah/GitHub/TAH-ROBOT/camera')
+from camera import Camera
+
+def robot_see():
+    my_camera = Camera()
+    my_camera.start()
+    #my_camera.deinit()
+
+async def gamepad_control() :
+    my_servos  = Servos()
+    my_motors  = Motors()
+    my_gamepad = Gamepad(servos_instance=my_servos, motors_instance=my_motors)
+
+    vision_process = multiprocessing.Process(target=robot_see, args=())
+    vision_process.start()
+
+    gamepad_loop   = asyncio.get_running_loop()
+    future = await asyncio.gather(my_gamepad.run_00(),my_gamepad.run_01())  
+    
+    vision_process.terminate()
+    my_servos.deinit()
+    my_motors.deinit()
+
+if __name__ == "__main__":
+    asyncio.run(gamepad_control())
+
+
+
+
