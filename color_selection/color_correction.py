41b3a2df6dafad57f45cd58850a50650840fef38
tharroun <thad.harroun@gmail.com>
2025-09-07 13:23:20 -0400

    contour

--- color_selection/color_correction.py
+++ color_selection/color_correction.py
@@ -52,13 +52,9 @@ class Camera:
     def start(self):
         t1 = time.perf_counter() 
         while True:
-            im  = self.picam2.capture_array()
-            res = cv2.resize(im,(596,324),interpolation = cv2.INTER_CUBIC)
-            #res = cv2.resize(im,(800,480),interpolation = cv2.INTER_CUBIC)
-
-            t2 = time.perf_counter()
-            #print(1/(t2-t1))
-            t1 = t2
+            image  = self.picam2.capture_array()
+            frame = cv2.resize(image,(596,324),interpolation = cv2.INTER_CUBIC)
+            #frame = cv2.resize(im,(800,480),interpolation = cv2.INTER_CUBIC)
             
             l_h = cv2.getTrackbarPos('LH','Camera')
             l_s = cv2.getTrackbarPos('LS','Camera')
@@ -71,12 +67,30 @@ class Camera:
             l_b = numpy.array([l_h, l_s, l_v])
             u_b = numpy.array([u_h, u_s, u_v])
 
-            res_hsv  = cv2.cvtColor(res, cv2.COLOR_BGR2HSV_FULL)
-            color_mask = cv2.inRange(res_hsv, l_b, u_b)
+            frame_hsv  = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV_FULL)
+            color_mask = cv2.inRange(frame_hsv, l_b, u_b)
             color_mask = cv2.morphologyEx(color_mask, cv2.MORPH_CLOSE, small_kernel, iterations = 1)
             color_mask = cv2.morphologyEx(color_mask, cv2.MORPH_OPEN, small_kernel,iterations = 1)
 
-            cv2.imshow("Camera", color_mask)
+            contours,hierarchy = cv2.findContours(color_mask, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            if (contours):
+                t2 = time.perf_counter()
+                if (len(contours)>1) : 
+                    contours = sorted(contours, key=lambda x: cv2.contourArea(x), reverse=True)
+                # ------------------------------------------------
+                #area = cv2.contourArea(contours[0])
+                #arclength = cv2.arcLength(contours[0],True)
+                #circularity = 4*numpy.pi*area/(arclength*arclength)
+                # NEXT TIME:https://docs.opencv.org/4.11.0/d4/d70/tutorial_hough_circle.html
+                # ------------------------------------------------
+                M = cv2.moments(contours[0])
+                cx = int(M['m10']/M['m00'])
+                cy = int(M['m01']/M['m00'])
+                radius = int(numpy.sqrt(M['m00']/numpy.pi))
+                if radius > 10:
+                    cv2.drawContours(frame,contours,0,(0,0,255),5)
+                t1 = t2
+            cv2.imshow("Camera", frame)
 
             if cv2.waitKey(1)==ord('q'):
                 break
