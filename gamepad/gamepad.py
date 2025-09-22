b219ea50dc44b0dc36d0bc98d0ce5ad138713f58
tharroun <thad.harroun@gmail.com>
2025-09-21 20:09:35 -0400

    REfactor complete

--- gamepad/gamepad.py
+++ gamepad/gamepad.py
@@ -4,15 +4,9 @@ import evdev
 import math
 import sys
 import asyncio
-from  multiprocessing import JoinableQueue
-import signal
-import functools
-import pprint
+from   multiprocessing import JoinableQueue
 sys.path.append('/home/tah/GitHub/TAH-ROBOT')
-sys.path.append('/home/tah/GitHub/TAH-ROBOT/servos')
-from servos import Servos
-sys.path.append('/home/tah/GitHub/TAH-ROBOT/motors')
-from motors import Motors
+
 
 class Gamepad:
     '''
@@ -20,10 +14,7 @@ class Gamepad:
     Appears as X-box controller on the RPi5.
     '''
     def __init__(
-        self, 
-        servos_instance : Servos | None = None,
-        motors_instance : Motors | None = None,
-        battery_queue   : type[JoinableQueue] | None = None,
+        self,
     ):
 
         found_gamepad = False
@@ -43,20 +34,9 @@ class Gamepad:
         while self.gamepad.read_one():
             pass
 
-        # - MOTOR BATTERY  -----
-        self.battery_message = False
-        if battery_queue != None : 
-            self.battery_queue = battery_queue
-            self.battery_message = True
+        self.running = True
 
         # - SERVOS -----
-        self.control_servos = True
-        if servos_instance != None : 
-            self.servos = servos_instance
-        else : 
-            raise RuntimeError(f"Must supply a valid servo instance, or set control_servos to False.")
-        self.servos.servo0.angle = 90
-        self.servos.servo1.angle = 90
         # RIGHT STICK
         # LINEAR MAPING OF STICK (MIN,MAX) -> (0,180) PWM VALUES
         absinfo = self.gamepad.absinfo(evdev.ecodes.ABS_RX)
@@ -67,11 +47,6 @@ class Gamepad:
         self.servos_by = -self.servos_my*absinfo.min
         
         # - MOTORS -----
-        self.control_motors = True
-        if motors_instance != None :
-            self.motors = motors_instance
-        else :
-            raise RuntimeError(f"Must supply a valid motors instance, or set control_motors to False.")
         # LEFT STICK
         # LINEAR MAPING OF STICK (0,ABS(MIN,MAX)) -> (0,1200) PWM SPEED UNITS
         #self.gamepad.set_absinfo(evdev.ecodes.ABS_X,flat=10,fuzz=20)
@@ -79,107 +54,48 @@ class Gamepad:
         absinfo = self.gamepad.absinfo(evdev.ecodes.ABS_X)
         if math.fabs(absinfo.max) > math.fabs(absinfo.min) : max = math.fabs(absinfo.max)
         else : max = math.fabs(absinfo.min)
-        self.motors_mx = 1200.0/max
+        self.motors_mx = 1200.0/math.fabs(max)
         absinfo = self.gamepad.absinfo(evdev.ecodes.ABS_Y)
         if math.fabs(absinfo.max) > math.fabs(absinfo.min) : max = math.fabs(absinfo.max)
         else : max = math.fabs(absinfo.min)
         self.motors_my = 1200.0/max
         self.rotation_speed = 300
-        
-# -----------------------------------
-
-# -----------------------------------
-    async def event_loop(self) -> bool:
-        sx=0
-        sy=0
-        async for event in self.gamepad.async_read_loop() :
-            if event.code == evdev.ecodes.BTN_MODE and event.value == 0:
-                self.control_motors = False
-                break
-            # Left joystick controls the motors
-            # We have a linar mapping of speed, we need to find the direction.
-            # Right joytick controls the servos.
-            if event.type ==  evdev.ecodes.EV_ABS :
-                if event.code == evdev.ecodes.ABS_RY : 
-                    sy=event.value
-                    self._update_servos(sx,sy)
-                if event.code == evdev.ecodes.ABS_RX : 
-                    sx=event.value
-                    self._update_servos(sx,sy)
-                if event.code == evdev.ecodes.ABS_HAT0Y :
-                    if event.value ==  1 : self.rotation_speed -= 50
-                    if event.value == -1 : self.rotation_speed += 50
-                if self.rotation_speed > 900 : self.rotation_speed = 900
-                if self.rotation_speed < 0   : self.rotation_speed = 0
-        self.gamepad.close()
-        print("Finished event_loop")
-        return True
-# -----------------------------------
-
-# -----------------------------------
-    def _update_servos(self,
-                       sx: float = 0,
-                       sy: float= 0) :
-        deg_x = math.trunc(self.servos_mx*sx+self.servos_bx)
-        deg_y = math.trunc(self.servos_my*sy+self.servos_by)
-        #print(deg_x,deg_y)
-        self.servos.servo0.angle = deg_x
-        self.servos.servo1.angle = deg_y
         return
+    
+    def deinit(self):
+        self.running = False
+        self.gamepad.close()
 # -----------------------------------
 
-# -----------------------------------
-    async def drive_loop(self) -> bool:
-        omega = 0
-        while self.control_motors :
-            mx = self.gamepad.absinfo(evdev.ecodes.ABS_X).value
-            my = self.gamepad.absinfo(evdev.ecodes.ABS_Y).value
-            
-            rcw  = self.gamepad.absinfo(evdev.ecodes.ABS_RZ).value
-            rccw = self.gamepad.absinfo(evdev.ecodes.ABS_Z).value
-            if rcw == 255 and rccw == 0   : omega = self.rotation_speed
-            elif rcw == 0 and rccw == 255 : omega = -self.rotation_speed
-            else : omega = 0
-
-            speed_x = self.motors_mx*math.fabs(mx)
-            speed_y = self.motors_my*math.fabs(my)
-            speed = math.sqrt(speed_x*speed_x+speed_y*speed_y)
-            direction = math.atan2(mx,my)*180.0/math.pi + 180.0
-            self.motors.go(speed,direction,omega)
-            await asyncio.sleep(0.1)
-        self.motors.stop()
-        print("Finished drive_loop")
-        return True
-# -----------------------------------
-
+running = True
 
 # -----------------------------------
-    async def battery_loop(self) -> bool:
-        while self.control_motors :
-            volts = self.motors.get_battery()
-            if self.battery_message : 
-                self.battery_queue.put(volts) # type: ignore
-            else :
-                print(f"Motor voltage: {volts}")
-            await asyncio.sleep(2.0)
-        print("Finished battery_loop")
-        return True
-#-----------------------------------
+async def listen_00(dev : Gamepad):
+    global running
+    async for ev in dev.gamepad.async_read_loop():
+        print("00:",evdev.categorize(ev),ev.value)
+        if ev.code == evdev.ecodes.BTN_MODE and ev.value == 0:
+            running = False
+            break
 
+# -----------------------------------
+async def listen_01(dev : Gamepad):
+    global running
+    while running:
+        r = dev.gamepad.absinfo(evdev.ecodes.ABS_X).value
+        print("01:",r)
+        await asyncio.sleep(0.05)
+
+# -----------------------------------
 async def gamepad_control() :
-    my_servos  = Servos()
-    my_motors  = Motors()
-    my_gamepad = Gamepad(servos_instance=my_servos, 
-                         motors_instance=my_motors,
-                         battery_queue=None)
+    my_gamepad = Gamepad()
 
     loop   = asyncio.get_running_loop()
-    future = await asyncio.gather(my_gamepad.event_loop(),
-                                  my_gamepad.drive_loop(),
-                                  my_gamepad.battery_loop())  
-    
-    my_servos.deinit()
-    my_motors.deinit()
+    future = await asyncio.gather(listen_00(my_gamepad),
+                                  listen_01(my_gamepad))  
+
+    my_gamepad.deinit()
+# -----------------------------------
 
 if __name__ == "__main__":
     asyncio.run(gamepad_control())
