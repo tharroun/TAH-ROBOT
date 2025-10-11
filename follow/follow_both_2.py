0363328c043d7b4d26f9b66494d52dcd230f1ed4
tharroun <thad.harroun@gmail.com>
2025-10-11 10:23:52 -0400

    add z movement

--- follow/follow_both_2.py
+++ follow/follow_both_2.py
@@ -255,13 +255,14 @@ def robot_move(servos_instance : Servos,
             #---
             if (i==4) :
                 omega = 0
+                move_z = pidz.pid(40, data[2], data[3])
                 # object left and looking left 
                 if new_x < 30 and servos_instance.servo0.angle < 45 :
                     omega = -pido.pid(cX,data[0],data[3])
                 # object right and looking right 
                 if new_x > 150 and servos_instance.servo0.angle > 135 : 
                     omega = -pido.pid(cX,data[0],data[3])
-                motors_instance.go(0,0,omega)
+                motors_instance.go(move_z,0,omega)
                 i=0
             else : i+=1
             #---
